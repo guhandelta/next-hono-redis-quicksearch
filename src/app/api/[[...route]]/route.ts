4e2f2e4c84d97b4ba1df63b522ed61b5709a5cff
Guhaprasaanth Nandagopal <guhan.delta@gmail.com>
2024-06-04 22:30:29 -0400

    Minor Bugfix in the route.ts

--- src/app/api/[[...route]]/route.ts
+++ src/app/api/[[...route]]/route.ts
@@ -26,8 +26,8 @@ app.get("/search", async (c) => {
 
         // Instantiate a Redis instance to communicate with the Redis DB, inside of this API route
         const redis = new Redis({
-            url: UPSTASH_REDIS_REST_URL,
-            token: UPSTASH_REDIS_REST_TOKEN
+            url: 'https://amusing-lemming-53777.upstash.io',
+            token: 'AdIRAAIncDFlOTJiZTFmMWI3NGI0Zjc3OWZhNzkwMGY5ZjUxOTViOXAxNTM3Nzc'
         });
 
         // Getting access to the search query from the client/UI
@@ -40,25 +40,31 @@ app.get("/search", async (c) => {
         const res = [];
         /* zrank gives the rank of the query in the sorted set, if it exists, else it returns null
         C - CO - COI - COIM - COIMB - COIMBA - COIMBAT - COIMBATO - COIMBATORE => COIMBATORE* | each item in the sorted set has a score and the rank is the position(how many-th item) of the item in the sorted set
-        The Query is case sensitive, so the query is converted to uppercase, to make the search much easier, without worrying about the case of the query
-        */
-        const rank = await redis.zrank("districts", query);
+        The Query is case sensitive, so the query is converted to uppercase, to make the search much easier, without worrying about the case of the query*/
+        const rank = await redis.zrank("districts", query.toLocaleUpperCase());
 
         if(rank !== null || rank !== undefined){
             // zrange gives the items in the sorted set within the range of the rank and the rank+50 (i.e.) 100 items from the element at the current rank | Eg: when typing Coi, look in all the elements, in the next 100 elements from the element in the current rank to find the complete name COIMBATORE* with the *
             const temp = await redis.zrange<string[]>( "districts", Number(rank), rank!+100 ); // Returns a string array with all the possible combinations of the district name
+            console.log("Temp:\t",temp);
 
             for(const el of temp){
-                if(!el.endsWith(query)) break;
-
+                
+                if(!el.startsWith(query)) break;
+                
                 // Send back the entire word without the *
                 if(el.endsWith("*")) {
+                    console.log("El:\t",el.substring(0, el.length - 1));
+                    
                     res.push(el.substring(0, el.length - 1));
                 }
+                console.log("Res:\t",res);
+                
             }
         }
 
         const end = performance.now();
+        
 
         return c.json({ duration: end - start, results: res });
     } catch (error) {
